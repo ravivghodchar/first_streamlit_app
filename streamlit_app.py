78d49ce3c8477a2c0f93a62396ca12658bb4d7ac
Ravi Ghodchar <104981447+ravivghodchar@users.noreply.github.com>
2022-05-10 22:36:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,17 @@ streamlit.dataframe(fruityvice_normalized)
  
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# Allow the end user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'kiwi')
+streamlit.write('Thanks for entering ',add_my_fruit)
+
+import requests
+fruityvice_response=requests.put("https://fruityvice.com/api/fruit/" + add_my_fruit)
