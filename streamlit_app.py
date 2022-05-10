423ea380ba601376a3dffa420829ee3543fae1e1
Ravi Ghodchar <104981447+ravivghodchar@users.noreply.github.com>
2022-05-11 00:37:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib import URLError
+from urllib.error import URLError
 
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
