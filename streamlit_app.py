4e41cbd89710799513c836a9da9208f636a454c9
Ravi Ghodchar <104981447+ravivghodchar@users.noreply.github.com>
2022-05-10 22:54:33 +0530

    Update streamlit_app.py
    his will not work correctly but just go with it

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,6 @@ streamlit.write('Thanks for entering ',add_my_fruit)
 
 import requests
 fruityvice_response=requests.put("https://fruityvice.com/api/fruit/" + add_my_fruit)
+
+#This will not work correctly but just go with it
+my_cur.execute("insert into fruit_load_list values ('from streamlit') ")
